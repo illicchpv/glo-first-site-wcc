a481ed3c670931004ca43fbdc1e04aefa651abff
illicchpv <illich58@mail.ru>
2026-07-29 16:00:28 +0300

    +Урок 4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('wcc:all-components-ready', () => {
   console.log('wcc:all-components-ready');
-  new WOW().init();
+
+  // new WOW().init();
+
   const swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.projects-pagination',
@@ -9,11 +11,11 @@ window.addEventListener('wcc:all-components-ready', () => {
       bulletActiveClass: 'projects-bullet-active',
       clickable: true,
     },
-    loop: true, // включаем бесконечный цикл
-    autoplay: {
-      delay: 3000, // Delay between transitions (in ms)
-      disableOnInteraction: false, // Continue autoplay after user interaction
-    },
+    // loop: true, // включаем бесконечный цикл
+    // autoplay: {
+    //   delay: 3000, // Delay between transitions (in ms)
+    //   disableOnInteraction: false, // Continue autoplay after user interaction
+    // },
   });
 });
 
